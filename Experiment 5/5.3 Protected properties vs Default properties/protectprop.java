0611db16ba5b505c6b0c7eedd7e6900921444c68
NihilSankar <111497481+NihilSankar@users.noreply.github.com>
2022-09-20 20:52:09 +0530

    Add files via upload

---
+++ Experiment 5/5.3 Protected properties vs Default properties/protectprop.java
@@ -0,0 +1,11 @@
+package protectprop;
+import protectprop.protectdefault.protectdefault;
+public class protectprop extends protectdefault
+{
+  public static void main(String[] args)
+  {
+    protectprop p=new protectprop();
+    p.get();
+    p.put();
+  }
+}
